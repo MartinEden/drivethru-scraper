f03511aa210a587859d810822bd5011deaa19c08
Martin Eden <MartinEden@users.noreply.github.com>
2026-05-27 15:23:51 +0100

    Make sure we get modules that are categorised as Classic D&D/AD&D but not any particular rulesystem nor labelled as OSR compatible.

--- src/main/kotlin/models/RuleSystem.kt
+++ src/main/kotlin/models/RuleSystem.kt
@@ -3,10 +3,11 @@ package eden.drivethru.models
 data class RuleSystem(val id: String, val name: String) {
     companion object {
         val osr = RuleSystem("45582", "Old-School Revival (OSR)")
+        val miscClassic = RuleSystem("1300", "Classic D&D/AD&D")
         val basic = RuleSystem("44828", "Basic/BECMI or OD&D Titles")
         val adnd1E = RuleSystem("44829", "AD&D 1E")
         val adnd2E = RuleSystem("44830", "AD&D 2E")
 
-        val all = listOf(osr, basic, adnd1E, adnd2E)
+        val all = listOf(osr, basic, adnd1E, adnd2E, miscClassic)
     }
 }
